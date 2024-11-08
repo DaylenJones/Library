5f7e80b5217299a06494e818e666a0df8337c6e6
Daylen Jones <jonesday001@warren.k12.in.us>
2024-11-08 13:17:10 -0500

    up

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Library {
+public class Library {
+
+    public String name;
+    public static int totalBooks = 0;
+
+
+    public void addBooks(int numBooks) {
+        totalBooks += numBooks; /
+    }
+
+
+    public static int getTotalBooks() {
+        return totalBooks;
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+
+
+        Library libraryA = new Library();
+        libraryA.name = "Library A";
+        libraryA.addBooks(100);
+
+        /
+        Library libraryB = new Library();
+        libraryB.name = "Library B";
+        libraryB.addBooks(150);
+
+
+        System.out.println("Total books in all libraries: " + Library.getTotalBooks());
+    }
 }
